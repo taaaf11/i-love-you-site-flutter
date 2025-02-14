2ba6c6a7dcecc3f500bcaea603dabfc4498c3c93
taaaf11 <taafuuu@gmail.com>
2025-02-14 23:00:24 +0500

    Add heart icons (source: favicon.io)

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'I love you!',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'I love you!'),
     );
   }
 }
